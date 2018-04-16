89d2af01ea75c637e4c907bba5e673beac75a718
Max Frischknecht <max@debutdebut.com>
2018-04-16 21:08:11 +0200

    polygon move + sliders
    make the polygon move, add sliders for vertises, height, amount, radius, period, offset

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
 let cnv;
-let radius = 20;
+let radius = 2;
 let vertises = 5;
-let strokeW;
+
+let inc;
+let wave = 0;
+let waveHeight = 60;
+let waveFreq = 0;
+let period = 1;
+let polygonAmount = 100;
+
 
 function setup() {
 	cnv = createCanvas(innerWidth, innerHeight);
 	cnv.parent("p5Sketch");
+	inc = PI / 30;
+	strokeWeight(5);
 }
 
 function draw() {
@@ -13,23 +22,51 @@ function draw() {
 	noFill();
 	stroke(100, 0, 255);
 
-	let value = document.getElementById("vertises").value;
-	let size = document.getElementById("size").value;
-	strokeW = document.getElementById("strokeW").value;
+	vertises = document.getElementById("slider1").value;
+	waveHeight = document.getElementById("slider2").value;
+	polygonAmount = document.getElementById("slider3").value;
+	radius = document.getElementById("slider4").value;
+	period = document.getElementById("slider5").value;
+	let offset = document.getElementById("slider6").value;
 
-	vertises = value;	
-	radius = size;
-
-	for(let i = 0; i < 10; i++){
-		polygon(vertises, radius * i, width/2, height/2);
+	for(let i = 0; i < polygonAmount; i++){
+		// the period changes the amount of waves
+		waveFreq = map(i, 0, polygonAmount, 0, TWO_PI * period);
+		// sin always between 0, 1, add height/2 to place it in the middle
+		// multiply it with the height you like the wave to be
+		// change the y position constantly
+		// add waveFreq to make each one different
+		let y = (height/2) + sin(wave + waveFreq) * waveHeight;
+		polygon(vertises, radius * i, width/2, y);
 	}
+
+
+	// for(let i = 0; i < polygonAmount; i++){
+	// 	waveFreq = map(i, 0, polygonAmount, 0, TWO_PI * period);
+	// 	let y = (height/2) + sin(wave + waveFreq) * waveHeight;
+	// 	pointIgon(vertises, radius * i, offset * i, width/2, y);
+	// }
+
+	// always add a 0.14... to it
+	wave += inc;
 }
 
 function polygon(vert, r, posX, posY) {
 	beginShape();
-	strokeWeight(strokeW);
 	for(let i = 0; i < vert; i++){
 		let angle = map(i, 0, vert, 0, TWO_PI);
+		// cos, sin return 0 and 1, thats why you must multiply it by its radius
+		let x = cos(angle) * r + posX;
+		let y = sin(angle) * r/2 + posY;
+		vertex(x, y);
+	}
+	endShape(CLOSE);
+}
+
+function pointIgon(vert, r, offset, posX, posY) {
+	beginShape(POINTS);
+	for(let i = 0; i < vert; i++){
+		let angle = map(i, 0, vert, 0 + offset, TWO_PI + offset);
 		// cos, sin return 0 and 1, thats why you must multiply it by its radius
 		let x = cos(angle) * r + posX;
 		let y = sin(angle) * r/2 + posY;
