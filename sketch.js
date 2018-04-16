01caddfbca99f1e90d5e89f7c378c6e9e1f42a32
Max Frischknecht <max@debutdebut.com>
2018-04-16 20:06:15 +0200

    additional sliders + styling
    more slider for size and strokeWeight, additional styling of the slider background

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let cnv;
 let radius = 20;
 let vertises = 5;
+let strokeW;
 
 function setup() {
 	cnv = createCanvas(innerWidth, innerHeight);
@@ -10,10 +11,14 @@ function setup() {
 function draw() {
 	background(51);
 	noFill();
-	stroke(0, 0, 255);
+	stroke(100, 0, 255);
 
 	let value = document.getElementById("vertises").value;
+	let size = document.getElementById("size").value;
+	strokeW = document.getElementById("strokeW").value;
+
 	vertises = value;	
+	radius = size;
 
 	for(let i = 0; i < 10; i++){
 		polygon(vertises, radius * i, width/2, height/2);
@@ -22,11 +27,12 @@ function draw() {
 
 function polygon(vert, r, posX, posY) {
 	beginShape();
+	strokeWeight(strokeW);
 	for(let i = 0; i < vert; i++){
 		let angle = map(i, 0, vert, 0, TWO_PI);
 		// cos, sin return 0 and 1, thats why you must multiply it by its radius
 		let x = cos(angle) * r + posX;
-		let y = sin(angle) * r + posY;
+		let y = sin(angle) * r/2 + posY;
 		vertex(x, y);
 	}
 	endShape(CLOSE);
